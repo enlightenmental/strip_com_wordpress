3246008b3cac6f8823e9f8aba20d942cc05d4438
Kyle Bahr <info@enlightenmental.com>
2014-12-15 15:56:39 +0000

    test comment 2 only

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  *
  * @package WordPress
  */
-
+ 
 /**
  * Tells WordPress to load the WordPress theme and output it.
  *
@@ -15,3 +15,5 @@ define('WP_USE_THEMES', true);
 
 /** Loads the WordPress Environment and Template */
 require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+
+
